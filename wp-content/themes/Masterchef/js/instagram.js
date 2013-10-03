276650f8a5e1cc96da36895d19c7e0b5e0dadcba
matt <matt@acmn.com.au>
2013-10-03 10:28:59 +1000

    Updates to make "More Photos" work on Instagram Page
    Signed-off-by: matt <matt@acmn.com.au>

--- wp-content/themes/Masterchef/js/instagram.js
+++ wp-content/themes/Masterchef/js/instagram.js
@@ -1,15 +1,13 @@
-if($('div.instagram')) {
 /* Find the access token: http://www.pinceladasdaweb.com.br/instagram/access-token */
 (function ($){
   $.fn.instagram = function (options) {
     var that = this,
         apiEndpoint = "https://api.instagram.com/v1",
-        settings = {
-        };
+        settings = {};
         
     options && $.extend(settings, options);
     
-    function createPhotoElement(photo) {
+     function createPhotoElement(photo) {
       var image_url = photo.images.low_resolution.url; // 306 x 306
       //var image_url = photo.images.thumbnail.url; // 150 x 150
       //var image_url = photo.images.standard_resolution.url; // 612 x 612
@@ -32,121 +30,6 @@ if($('div.instagram')) {
           .append(innerHtml);
 
       return $('<figure>')
-        .addClass('myImg')
-        .attr('id', photo.id)
-        .append(innerHtml)
-        .append(cap_details);
-    }
-    
-    function createEmptyElement() {
-      return $('<div>')
-        .addClass('instagram-placeholder')
-        .attr('id', 'empty')
-        .append($('<p>').html('No photos for this query'));
-    }
-    
-    
-    settings.onLoad != null && typeof settings.onLoad == 'function' && settings.onLoad();
-    
-    $.ajax({
-      type: "GET",
-      dataType: "jsonp",
-      cache: false,
-      url: 'https://api.instagram.com/v1/users/self/media/liked?access_token=432324504.5b9e1e6.68a7b3abd5c9438f8afa65f107995f09',
-      success: function (res) {
-        var length = typeof res.data != 'undefined' ? res.data.length : 0;
-        var limit = settings.show != null && settings.show < length ? settings.show : length;
-        
-        if (limit > 0) {
-          for (var i = 0; i < limit; i++) {
-            that.append(createPhotoElement(res.data[i]));
-          }
-        }
-        else {
-          that.append(createEmptyElement());
-        }
-
-        settings.onComplete != null && typeof settings.onComplete == 'function' && settings.onComplete(res.data, res);
-      }
-    });
-    
-    return this;
-  };
-})(jQuery);
-
-
-}
-/*
-$('document').ready(function(){
-	$(function() {
-	  $(".instagram").instagram();	
-	 });
-});
-*/
-
-$(function(){
-  var
-    insta_container = $("div.instagram")
-  , insta_next_url
-
-  insta_container.instagram({
-      show : 18
-    , onComplete : function (photos, data) {
-      insta_next_url = data.pagination.next_url
-    }
-  })
-
-  $('button.more-instagram').on('click', function(){
-    var 
-      button = $(this)
-    , text = button.text()
-
-    if (button.text() != 'Loading…'){
-      button.text('Loading…')
-      insta_container.instagram({
-          next_url : insta_next_url
-        , show : 18
-        , onComplete : function(photos, data) {
-          insta_next_url = data.pagination.next_url
-          button.text(text)
-        }
-      })
-    }       
-  }) 
-});
-
-
-/* Find the access token: http://www.pinceladasdaweb.com.br/instagram/access-token */
-(function ($){
-  $.fn.instagram = function (options) {
-    var that = this,
-        apiEndpoint = "https://api.instagram.com/v1",
-        settings = {};
-        
-    options && $.extend(settings, options);
-    
-    function createPhotoElement(photo) {
-      var image_url = photo.images.low_resolution.url; // 306 x 306
-      //var image_url = photo.images.thumbnail.url; // 150 x 150
-      //var image_url = photo.images.standard_resolution.url; // 612 x 612
-      var likes = photo.likes.count;
-      var user = photo.user.username;
-      var username = photo.user.full_name;
-      var userurl = 'http://instagram.com/' + user;
-
-      var innerHtml = $('<img>')
-        .addClass('instagram-image')
-        .attr('src', image_url);
-      
-      
-      var cap_details = '<div><i class="likes entypo-heart"> ' + likes + '</i><i class="user"><a href="' + userurl + '" target="_blank">' + username + '</a></i></div>';
-  
-      innerHtml = $('<a>')
-          .attr('target', '_blank')
-          .attr('href', photo.link)
-          .append(innerHtml);
-
-      return $('<li>')
         .addClass('myImg')
         .attr('id', photo.id)
         .append(innerHtml)
--- wp-content/themes/Masterchef/page-instagram.php
+++ wp-content/themes/Masterchef/page-instagram.php
@@ -12,7 +12,7 @@
 					<h2>Instagram Feed <a href="http://instagram.com/MCDiningandBar" target="_blank">#mcdb</a></h2>
 				</header>
 				<div class="entry-content">
-				  <div class="instagram-list"></div>
+				  <div class="instagram"></div>
 				  <!-- <button class="more-instagram">Get More</button> -->
 				</div>
 			</article>
@@ -26,10 +26,10 @@
 	<?php
 				    $theurl = 'https://api.instagram.com/v1/users/self/media/liked?access_token=432324504.5b9e1e6.68a7b3abd5c9438f8afa65f107995f09';
             
-            if ( false === ( $result = get_transient( 'instragram_result' ) ) ) {
+            if ( false === ( $result = get_transient( 'instragram' ) ) ) {
               // It wasn't there, so regenerate the data and save the transient
               $result = wp_remote_get($theurl);
-              set_transient( 'instragram_result', $result, 60*60*12 );
+              set_transient( 'instragram', $result, 60*60*12 );
 
             }
             
@@ -41,39 +41,40 @@
               
                  
               function createPhotoElement(photo) {
-                var image_url = photo.images.low_resolution.url; // 306 x 306
-                //var image_url = photo.images.thumbnail.url; // 150 x 150
-                //var image_url = photo.images.standard_resolution.url; // 612 x 612
-                var likes = photo.likes.count;
-                var user = photo.user.username;
-                var username = photo.user.full_name;
-                var userurl = 'http://instagram.com/' + user;
-          
-                var innerHtml = $('<img>')
-                  .addClass('instagram-image')
-                  .attr('src', image_url);
-                
-                
-                var cap_details = '<div><i class="likes entypo-heart"> ' + likes + '</i><i class="user"><a href="' + userurl + '" target="_blank">' + username + '</a></i></div>';
+                  var image_url = photo.images.low_resolution.url; // 306 x 306
+                  //var image_url = photo.images.thumbnail.url; // 150 x 150
+                  //var image_url = photo.images.standard_resolution.url; // 612 x 612
+                  var likes = photo.likes.count;
+                  var user = photo.user.username;
+                  var username = photo.user.full_name;
+                  var userurl = 'http://instagram.com/' + user;
+                  var type = photo.type;
+                  
+                  var innerHtml = $('<img>')
+                    .addClass('instagram-' + type )
+                    .attr('src', image_url);
+                  
+                  
+                  var cap_details = '<figcaption><i class="likes icon-heart"> ' + likes + '</i><i class="user"><a href="' + userurl + '" target="_blank">' + username + '</a></i></figcaption>';
+              
+                  innerHtml = $('<a>')
+                      .attr('target', '_blank')
+                      .attr('href', photo.link)
+                      .append(innerHtml);
             
-                innerHtml = $('<a>')
-                    .attr('target', '_blank')
-                    .attr('href', photo.link)
-                    .append(innerHtml);
-          
-                return $('<li>')
-                  .addClass('myImg')
-                  .attr('id', photo.id)
-                  .append(innerHtml)
-                  .append(cap_details);
-              }
+                  return $('<figure>')
+                    .addClass('myImg')
+                    .attr('id', photo.id)
+                    .append(innerHtml)
+                    .append(cap_details);
+                }
             
             
             
               var stuff = <?php print json_encode($result); ?>;
               var url = <?php print json_encode($theurl); ?>;
               
-              var that = $('.instagram-list');
+              var that = $('div.instagram');
               
               var next_link = stuff.pagination.next_max_like_id;
               
@@ -85,7 +86,7 @@
                 that.append(createPhotoElement(photo));
               }
               
-              that.after('<a href="#" class="insta-next"  data-next="' + url + '&max_like_id=' + next_link + '">More Photos</a>');
+              that.after('<div style="clear:both; margin-top:20px; text-align:center;"><a href="#" class="button white insta-next"  data-next="' + url + '&max_like_id=' + next_link + '">More Photos</a></div>');
             
             
               $("body").on("click",".insta-next", function(event){
@@ -108,9 +109,9 @@
                               that.append(createPhotoElement(photo));
                             }
                             if(next_link){
-                              that.after('<a href="#" class="insta-next" data-next="' + url + '&max_like_id=' + next_link + '">More Photos</a>');
+                              that.after('<div style="clear:both; margin-top:20px; text-align:center;"><a href="#" class="button white insta-next" data-next="' + url + '&max_like_id=' + next_link + '">More Photos</a></div');
                               } else {
-                                that.after('<h4>That\'s it kids.<br>Follow us on Instragram for more...<br><a href="http://instagram.com/greaseoztour" target="_blank">@GreaseOzTour</a></h4>');
+                                that.after('<div style="clear:both; margin-top:20px; text-align:center;"><h4>You\'ve reached the end.<br>Follow us on Instragram for more...<br><a href="http://instagram.com/MCDiningandBar" target="_blank">@MCDiningandBar</a></h4></div>');
                               }
                           }
                           
